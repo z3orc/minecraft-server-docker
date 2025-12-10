735b2fe2133a6063a896298800393b19f7c49d68
z3orc <z3orc@users.noreply.github.com>
2025-12-10 17:46:58 +0100

    chore: remove unused code

--- main.go
+++ main.go
@@ -6,10 +6,6 @@ import (
 	"runtime"
 )
 
-func print_help() {
-	fmt.Printf("usage: %s <executable> <timeout>\n- executable: Path to server jar file\n- timeout: How long to for server to gracefully close\n", os.Args[0])
-}
-
 func main() {
 	if runtime.GOOS != "linux" {
 		fmt.Println("error: Program only works on Linux systems")
@@ -17,33 +13,21 @@ func main() {
 	}
 
 	flags := ParseFlags()
-	fmt.Println(flags)
+	fmt.Println("runner: flags=", flags)
 
 	server, err := NewServer(flags.JarPath)
 	if err != nil {
 		fmt.Printf("runner: Failed to initialize server: %e", err)
 		os.Exit(1)
 	}
+	fmt.Println("runner: server=", server)
 
 	server.RedirectStdout(os.Stdout)
 	server.SignalCatcher(flags.Timeout, flags.UseSigKill)
 
-	// if len(os.Args) < 3 {
-	// 	fmt.Println("error: Missing arguments!")
-	// 	print_help()
-	// 	os.Exit(1)
-	// }
-
-	// jar_path := os.Args[1]
-	// timeout, err := strconv.Atoi(os.Args[2])
-	// if err != nil {
-	// 	fmt.Println("error: Failed to parse value for timeout")
-	// 	print_help()
-	// 	os.Exit(1)
-	// }
-
-	fmt.Printf("runner: Starting server. jar=%s, timeout=%d, cpus=%d\n", flags.JarPath, flags.Timeout, runtime.NumCPU())
+	fmt.Printf("runner: Starting server. jar=%s, timeout=%d\n", flags.JarPath, flags.Timeout)
 	server.Run()
+
 	exitCode := server.ExitCode()
 	fmt.Println("runner: Server exited with code:", exitCode)
 	os.Exit(0)
