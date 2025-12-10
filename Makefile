c649b1e1df85c36ce8d1c91215db73fabf185eb6
z3orc <z3orc@users.noreply.github.com>
2025-12-10 15:23:38 +0100

    feat: use bufio to copy server stdout to stdout, react to signals with stop command

--- Makefile
+++ Makefile
@@ -3,6 +3,10 @@ all: build
 get-jar: build
 	wget https://fill-data.papermc.io/v1/objects/a61a0585e203688f606ca3a649760b8ba71efca01a4af7687db5e41408ee27aa/paper-1.21.10-117.jar -O ./out/server.jar
 
+run: build
+	cd ./out/ && ./runner server.jar
+
+
 build:
 	mkdir -p ./out/
 	go build -o ./out/runner
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
@@ -23,19 +24,46 @@ func main() {
 
 	jar_path := os.Args[1]
 	cmd := exec.Command("java", "-jar", jar_path, "-nogui")
-	cmd.Stdout = os.Stdout
-	cmd.Stdin = os.Stdin
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setpgid: true,
+	}
+	cmdStdin, err := cmd.StdinPipe()
+	if err != nil {
+		fmt.Printf("error: Failed to open Stdin Pipe: %e\n", err)
+		cmd.Process.Kill()
+		os.Exit(-1)
+	}
+
+	cmdStdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Printf("error: Failed to open Stdout Pipe: %e\n", err)
+		cmd.Process.Kill()
+		os.Exit(-1)
+	}
+
+	go func() {
+		scanner := bufio.NewScanner(cmdStdout)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
 
 	signalChannel := make(chan os.Signal, 1)
-	signal.Notify(signalChannel, syscall.SIGTERM, syscall.SIGINT)
+	signal.Notify(signalChannel, syscall.SIGTERM, syscall.SIGINT, os.Interrupt)
 
 	go func() {
 		signal := <-signalChannel
-		fmt.Println("Got signal: ", signal.String())
-		cmd.Process.Signal(syscall.SIGINT)
+		fmt.Println("runner: Got signal: ", signal.String())
+		fmt.Println("runner: Ignoring signal")
+		// cmd.Process.Signal(syscall.SIGINT)
+		fmt.Fprintln(cmdStdin, "stop")
 	}()
 
 	cmd.Run()
+
+	cmdStdin.Close()
+	cmdStdout.Close()
+
 	exitCode := cmd.ProcessState.ExitCode()
 	fmt.Println("runner: Server exited with code: ", exitCode)
 }
