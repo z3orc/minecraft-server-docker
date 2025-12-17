d761fbd670d3498f2d0fa5f7f1531b25132defd0
z3orc <z3orc@users.noreply.github.com>
2025-12-17 18:14:39 +0100

    feat: use new server struct to start server, avoid double logging

--- main.go
+++ main.go
@@ -4,27 +4,25 @@ import (
 	"flag"
 	"log/slog"
 	"os"
-	"path/filepath"
 	"runtime"
 
-	"github.com/z3orc/minecraft-server-docker/internal/fabric"
-	"github.com/z3orc/minecraft-server-docker/internal/jar"
 	"github.com/z3orc/minecraft-server-docker/internal/logger"
-	"github.com/z3orc/minecraft-server-docker/internal/properties"
-	"github.com/z3orc/minecraft-server-docker/internal/serverexec"
+	"github.com/z3orc/minecraft-server-docker/internal/server"
 )
 
 type flags struct {
-	dataDir    string
-	JarName    string
-	Timeout    int
-	UseSigKill bool
-	Debug      bool
+	gameVersion string
+	dataDir     string
+	JarName     string
+	Timeout     int
+	UseSigKill  bool
+	Debug       bool
 }
 
 func parseFlags() *flags {
 	flags := flags{}
 
+	gameVersionPtr := flag.String("version", "1.21.10", "Which version of minecraft the server should run. Only works if there does not already exist a server.jar file")
 	dataDirPtr := flag.String("dir", "./", "Directory of server files. This should be the same location as the server jar.")
 	jarNamePtr := flag.String("jar", "server.jar", "Name of server jar that the runner will use")
 	timeoutPtr := flag.Int("timeout", 60, "How long to wait for the server to gracefully shut down (in seconds)")
@@ -33,6 +31,7 @@ func parseFlags() *flags {
 
 	flag.Parse()
 
+	flags.gameVersion = *gameVersionPtr
 	flags.dataDir = *dataDirPtr
 	flags.JarName = *jarNamePtr
 	flags.Timeout = *timeoutPtr
@@ -57,50 +56,56 @@ func main() {
 		logger.SetDebugLogLevel()
 	}
 
-	props := properties.New(filepath.Join(flags.dataDir, "server.properties"))
-	err := props.LoadFromEnv()
+	// props := properties.New(filepath.Join(flags.dataDir, "server.properties"))
+	// err := props.LoadFromEnv()
+	// if err != nil {
+	// 	slog.Error("failed to load values for server.properties from env:", "err", err)
+	// 	os.Exit(1)
+	// }
+
+	// err = props.Write()
+	// if err != nil {
+	// 	slog.Error("failed to write values for server.properties to disk:", "err", err)
+	// 	os.Exit(1)
+	// }
+
+	// url, err := fabric.GetDownloadUrl("1.21.10")
+	// if err != nil {
+	// 	slog.Error("failed get download url from fabric:", "err", err)
+	// 	os.Exit(1)
+	// }
+
+	// err = jar.DownloadServerJar(url, flags.dataDir)
+	// if err != nil {
+	// 	slog.Error("error while downloading server jar", "err", err)
+	// 	os.Exit(1)
+	// }
+
+	// server, err := serverexec.New(flags.dataDir, flags.JarName)
+	// if err != nil {
+	// 	slog.Error("failed to initialize server:", "err", err)
+	// 	os.Exit(1)
+	// }
+	// slog.Debug("current value of server", "server", server)
+
+	// server.RedirectStdout(os.Stdout)
+	// server.SignalCatcher(flags.Timeout, flags.UseSigKill)
+
+	slog.Debug("flags", "version", flags.gameVersion, "dir", flags.dataDir, "jar", flags.JarName, "timeout", flags.Timeout, "useSigKill", flags.UseSigKill, "debug", flags.Debug)
+	s, err := server.New(flags.gameVersion, flags.dataDir, flags.JarName, flags.Timeout, flags.UseSigKill)
 	if err != nil {
-		slog.Error("failed to load values for server.properties from env:", "err", err)
+		slog.Error("failed to create server instance:", "err", err)
 		os.Exit(1)
 	}
 
-	err = props.Write()
-	if err != nil {
-		slog.Error("failed to write values for server.properties to disk:", "err", err)
-		os.Exit(1)
-	}
-
-	url, err := fabric.GetDownloadUrl("1.21.10")
-	if err != nil {
-		slog.Error("failed get download url from fabric:", "err", err)
-		os.Exit(1)
-	}
-
-	err = jar.DownloadServerJar(url, flags.dataDir)
-	if err != nil {
-		slog.Error("error while downloading server jar", "err", err)
-		os.Exit(1)
-	}
-
-	server, err := serverexec.New(flags.dataDir, flags.JarName)
-	if err != nil {
-		slog.Error("failed to initialize server:", "err", err)
-		os.Exit(1)
-	}
-	slog.Debug("current value of server", "server", server)
-
-	server.RedirectStdout(os.Stdout)
-	server.SignalCatcher(flags.Timeout, flags.UseSigKill)
-
-	slog.Info("starting server", "dir", flags.dataDir, "jar", flags.JarName, "timeout", flags.Timeout)
-	err = server.Run()
+	err = s.Start()
 	if err != nil {
 		slog.Error("failed to start server:", "err", err)
 		os.Exit(1)
 	}
 
-	exitCode := server.ExitCode()
-	slog.Info("server exited", "exit code", exitCode)
+	// exitCode := server.ExitCode()
+	// slog.Info("server exited", "exit code", exitCode)
 
 	os.Exit(0)
 }
