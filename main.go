4a7276edce4230ff8a0b7f1446881213090dcb7a
z3orc <z3orc@users.noreply.github.com>
2025-12-18 17:50:31 +0100

    refactor: make values in struct flags private, avoid storing flags as variables

--- main.go
+++ main.go
@@ -13,30 +13,36 @@ import (
 type flags struct {
 	gameVersion string
 	dataDir     string
-	JarName     string
-	Timeout     int
-	UseSigKill  bool
-	Debug       bool
+	jarName     string
+	timeout     int
+	useSigKill  bool
+	debug       bool
 }
 
 func parseFlags() *flags {
 	flags := flags{}
 
-	gameVersionPtr := flag.String("version", "1.21.10", "Which version of minecraft the server should run. Only works if there does not already exist a server.jar file")
-	dataDirPtr := flag.String("dir", "./", "Directory of server files. This should be the same location as the server jar.")
-	jarNamePtr := flag.String("jar", "server.jar", "Name of server jar that the runner will use")
-	timeoutPtr := flag.Int("timeout", 60, "How long to wait for the server to gracefully shut down (in seconds)")
-	useSigKillPtr := flag.Bool("sigkill", false, "Use signal SIGKILL to close server if timeout is reached")
-	debugPtr := flag.Bool("debug", false, "Use debug mode")
+	flag.StringVar(&flags.gameVersion, "version", "1.21.10",
+		"Which version of minecraft the server should run. "+
+			"Only works if there does not already exist a server.jar file")
+
+	flag.StringVar(&flags.dataDir, "dir", "./",
+		"Directory of server files. This should be the same location as the server jar.")
+
+	flag.StringVar(&flags.jarName, "jar", "server.jar",
+		"Name of server jar that the runner will use")
+
+	flag.IntVar(&flags.timeout, "timeout", 60,
+		"How long to wait for the server to gracefully shut down (in seconds)")
+
+	flag.BoolVar(&flags.useSigKill, "sigkill", false,
+		"Use signal SIGKILL to close server if timeout is reached")
+
+	flag.BoolVar(&flags.debug, "debug", false, "Use debug mode")
 
 	flag.Parse()
 
-	flags.gameVersion = *gameVersionPtr
-	flags.dataDir = *dataDirPtr
-	flags.JarName = *jarNamePtr
-	flags.Timeout = *timeoutPtr
-	flags.UseSigKill = *useSigKillPtr
-	flags.Debug = *debugPtr
+	println("sigkill:", flags.useSigKill)
 
 	return &flags
 }
@@ -50,11 +56,10 @@ func main() {
 	}
 
 	flags := parseFlags()
-	slog.Debug("current value of flags", "flags", flags)
-
-	if flags.Debug {
+	if flags.debug {
 		logger.SetDebugLogLevel()
 	}
+	slog.Debug("value of flags", "flags", flags)
 
 	// props := properties.New(filepath.Join(flags.dataDir, "server.properties"))
 	// err := props.LoadFromEnv()
@@ -91,8 +96,8 @@ func main() {
 	// server.RedirectStdout(os.Stdout)
 	// server.SignalCatcher(flags.Timeout, flags.UseSigKill)
 
-	slog.Debug("flags", "version", flags.gameVersion, "dir", flags.dataDir, "jar", flags.JarName, "timeout", flags.Timeout, "useSigKill", flags.UseSigKill, "debug", flags.Debug)
-	s, err := server.New(flags.gameVersion, flags.dataDir, flags.JarName, flags.Timeout, flags.UseSigKill)
+	slog.Debug("flags", "version", flags.gameVersion, "dir", flags.dataDir, "jar", flags.jarName, "timeout", flags.timeout, "useSigKill", flags.useSigKill, "debug", flags.debug)
+	s, err := server.New(flags.gameVersion, flags.dataDir, flags.jarName, flags.timeout, flags.useSigKill)
 	if err != nil {
 		slog.Error("failed to create server instance:", "err", err)
 		os.Exit(1)
